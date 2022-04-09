bad7cca351a096d3f36222e601b4fec901008e23
Eun Jin Kim <eunjinkim@macbook-pro.myfiosgateway.com>
2022-04-09 10:29:13 -0400

    upload

--- main.js
+++ main.js
@@ -57,4 +57,59 @@ let lineChart = new Chart(
 
 //=====================================================================
 
-//polar area chart:
+//bar area chart:
+
+//create new div for bar chart
+let div3 = $('<div> </div>');
+let canvas2 = $('<canvas id="barChart"> </canvas>');
+div3.append(canvas2);
+
+
+//append div2 to body
+body.append(div3);
+
+
+//let labels = Utils.months({count: 7});
+let data2 = {
+  labels: months,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(201, 203, 207, 0.2)'
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',      
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(201, 203, 207)'
+    ],
+    borderWidth: 1
+  }]
+};
+
+let barConfig = {
+    type: 'bar',
+    data: data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    },
+  };
+
+let barChart = new Chart(
+    document.getElementById('barChart'),
+    barConfig
+);
